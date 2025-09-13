f6f15a2d7b057d66d3caebe52e3b7355ebd3446f
stutirawat100-ctrl <stutirawat100@gmail.com>
2025-09-13 23:07:04 +0530

    Create helloworld.c

---
+++ LAB-1/helloworld.c
@@ -0,0 +1,9 @@
+/* LAB-1 
+Q1   Write a C program to print "Hello World".*/
+
+#include <stdio.h>                                // Header file
+int main()
+{
+    printf("Hello World \n ");                   // Print message
+    return 0;
+}                                                // End program
