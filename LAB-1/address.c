d45f85fecbf9fa9bb1233f2b9a422d2bbdd8b565
stutirawat100-ctrl <stutirawat100@gmail.com>
2025-09-13 23:09:18 +0530

    Add files via upload

---
+++ LAB-1/address.c
@@ -0,0 +1,13 @@
+/* LAB-1
+Q2 Write a C program to print the address in multiple lines (new lines).*/
+
+#include <stdio.h>                          // Header file
+int main()
+{
+    // Print Address in multiple lines
+    printf("House no. 23, Lane no.2, \n");
+    printf("Upper Sarthi Vihar, PO: Nehrugram, \n");
+    printf("Dehradun - 248005. \n ");
+
+    return 0;
+}                                           // END program
